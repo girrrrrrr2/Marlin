a320ec8851b17bfa441f884fd8c86aaa847c426a
girrrrrrr2 <56775545+girrrrrrr2@users.noreply.github.com>
2020-10-30 18:03:46 -0700

    Adjusted probe offset
    This way i can go below what i set it as

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1077,7 +1077,7 @@
   #if HAS_BED_PROBE
     #define PROBE_OFFSET_WIZARD
     #if ENABLED(PROBE_OFFSET_WIZARD)
-      #define PROBE_OFFSET_START 0.0   // Estimated nozzle-to-probe Z offset, plus a little extra
+      #define PROBE_OFFSET_START -1.25   // Estimated nozzle-to-probe Z offset, plus a little extra
     #endif
   #endif
 
